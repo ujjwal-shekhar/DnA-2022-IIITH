419951dcbc1b420060b5a60c16f71be01eb6e185
araavind312 <ach.aditya312@gmail.com>
2022-11-26 18:16:38 +0530

    Anika values

--- dump.sql
+++ dump.sql
@@ -76,11 +76,31 @@ CREATE TABLE `Villagers` (
 --
 
 LOCK TABLES `Villagers` WRITE;
-/*!40000 ALTER TABLE `Villagers` DISABLE KEYS */;
-INSERT INTO `Villagers` VALUES ('Headquarters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
-/*!40000 ALTER TABLE `Villagers` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `Villagers` VALUES 
+('123456123411','ABCDEF123411','Dhruv Madan','Brahmin','Male',21,1234567811,'Illiterate','Nurse'),
+('123456123412','ABCDEF123412','Vasu Sood','Brahmin','Male',50,1234567812,'Illiterate','Farmer'),
+('123456123413','ABCDEF123413','Chandan Mishra','Kadia','Male',63,1234567813,'Literate','Farmer'),
+('123456123414','ABCDEF123414','Bhavana Bala','Kadia','Male',19,1234567814,'Literate','Doctor'),
+('123456123415','ABCDEF123415','Vinay Sen','Nair','Male',75,1234567815,'Illiterate','Secretary'),
+('123456123416','ABCDEF123416','Karthik Pillay','Dalit','Male',45,1234567816,'Illiterate','Businessman'),
+('123456123421','ABCDEF123421','Varsha Balay','Mahuri','Female',42,1234567821,'Literate','Farmer'),
+('123456123422','ABCDEF123422','Sona Bhattacharyya','Halba','Female',27,1234567822,'Literate','Farmer'),
+('123456123423','ABCDEF123423','Dipali Dora','Dalit','Female',38,1234567823,'Illiterate','Manager'),
+('123456123424','ABCDEF123424','Radha Bhatia','Mahuri','Female',29,1234567824,'Illiterate','Farmer'),
+('123456123425','ABCDEF123425','Durga Mathur','Iyer','Female',45,1234567825,'Literate','Housewife'),
+('123456123426','ABCDEF123426','Lal Dayal','Brahmin','Male',47,1234567826,'Literate','Teacher'),
+('123456123431','ABCDEF123431','Akhil Dey','Dalit','Male',67,1234567831,'Illiterate','Teacher'),
+('123456123432','ABCDEF123432','Abhilash Salvi','Vaishya','Male',90,1234567832,'Illiterate','Principal'),
+('123456123433','ABCDEF123433','Shivansh Mehan','Dalit','Male',83,1234567833,'Literate','Postman'),
+('123456123434','ABCDEF123434','Amrit Halder','Kshatriya','Male',101,1234567834,'Literate','Businessman'),
+('123456123435','ABCDEF123435','Rajni Shenoy','Brahmin','Female',19,1234567835,'Illiterate','Postmaster'),
+('123456123436','ABCDEF123436','Indrani Narula','Kshatriya','Female',18,1234567836,'Illiterate','Policeman'),
+('123456123441','ABCDEF123441','Roshan Kota','Dalit','Male',18,1234567841,'Literate','Farmer'),
+('123456123442','ABCDEF123442','Ayush Sen','Vaishya','Male',21,1234567842,'Literate','Businessman');
 
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
+UNLOCK TABLES;
 -------------------------------------------------------------------- RU END ------------------------------------------------------------------------
 
 -------------------------------------------------------------------- RU START ------------------------------------------------------------------------
@@ -101,10 +121,20 @@ CREATE TABLE `Panchayat_Members` (
   CONSTRAINT `Panchayat_Members_ibfk_1` FOREIGN KEY (`Supervisor_Aadhar_No`) REFERENCES `Panchayat_Members` (`Aadhar_No`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+
 LOCK TABLES `Panchayat_Members` WRITE;
-/*!40000 ALTER TABLE `Panchayat_Members` DISABLE KEYS */;
-INSERT INTO `Panchayat_Members` VALUES ('Headquarters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
-/*!40000 ALTER TABLE `Panchayat_Members` ENABLE KEYS */;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `Panchayat_Members` VALUES 
+/*sarpanch*/
+('123456123413','ABCDEF123413',22,3,20000,63,NULL),
+
+('123456123411','ABCDEF123411',2,1,10000,21,'1234567813'),
+('123456123412','ABCDEF123412',7,2,12000,50,'1234567813'),
+('123456123426','ABCDEF123426',12,4,15000,47,'1234567813'),
+('123456123431','ABCDEF123431',22,5,18000,67,'1234567813'),
+('123456123432','ABCDEF123432',12,6,15000,90,'1234567813');
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
 UNLOCK TABLES;
 
 -------------------------------------------------------------------- RU END ------------------------------------------------------------------------
@@ -132,9 +162,46 @@ CREATE TABLE `Taxation` (
 --
 
 LOCK TABLES `Taxation` WRITE;
-/*!40000 ALTER TABLE `Taxation` DISABLE KEYS */;
-INSERT INTO `Taxation` VALUES ('Headquarters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
-/*!40000 ALTER TABLE `Taxation` ENABLE KEYS */;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `Taxation` VALUES 
+/*personal_taxes*/
+('P23456123411','Personal',150000,50000,'123456123411'),
+/*('P23456123412','Personal','50000','0','123456123412'),*/
+-- ('P23456123413','Personal','400000','200000','123456123413'),
+('P23456123414','Personal',1000000,400000,'123456123414'),
+('P23456123415','Personal',400000,100000,'123456123415'),
+-- ('P23456123416','Personal','1000000','300000','123456123416'),
+-- ('P23456123421','Personal','300000','70000','123456123421'),
+-- ('P23456123422','Personal','15000','0','123456123422'),
+('P23456123423','Personal',200000,30000,'123456123423'),
+-- ('P23456123424','Personal','200000','80000','123456123424'),
+('P23456123425','Personal',0,0,'123456123425',),
+('P23456123426','Personal',250000,100000,'123456123426'),
+('P23456123431','Personal',150000,50000,'123456123431'),
+('P23456123432','Personal',300000,90000,'123456123432'),
+('P23456123433','Personal',40000,0,'123456123433'),
+-- ('P23456123434','Personal','400000','100000','123456123434'),
+('P23456123435','Personal',90000,30000,'123456123435'),
+('P23456123436','Personal',500000,200000,'123456123436'),
+-- ('P23456123441','Personal','1000000','400000','123456123441'),
+-- ('P23456123442','Personal','600000','150000','123456123442'),
+
+/*property tax on farmland*/
+('F23456123412','Property',50000,0,'123456123412'),
+('F23456123413','Property',400000,200000,'123456123413'),
+('F23456123421','Property',300000,70000,'123456123421'),
+('F23456123422','Property',15000,0,'123456123422'),
+('F23456123424','Property',200000,80000,'123456123424'),
+/*farmlands owned by 41*/
+('F23456123441','Property',1000000,400000,'123456123441'),
+('F33456123441','Property',1000000,400000,'123456123441'),
+
+/*business tax*/
+('B23456123416','Business',1000000,300000,'123456123416'),
+('B23456123434','Business',400000,100000,'123456123434'),
+('B23456123442','Business',600000,150000,'123456123442'),
+
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -209,10 +276,19 @@ CREATE TABLE `Farmlands` (
 --
 
 LOCK TABLES `Farmlands` WRITE;
-/*!40000 ALTER TABLE `Farmlands` DISABLE KEYS */;
-INSERT INTO `Farmlands` VALUES ('Headquarters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
-/*!40000 ALTER TABLE `Farmlands` ENABLE KEYS */;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `Farmlands` VALUES 
+('123456123412',1,500,'F23456123412'),
+('123456123413',1,400,'F23456123413'),
+('123456123421',1,300,'F23456123421'),
+('123456123422',1,150,'F23456123422'),
+('123456123424',1,200,'F23456123424'),
+/*farmlands owned by 41*/
+('123456123441',1,1000,'F23456123441'),
+('123456123441',2,5000,'F33456123441');
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
 UNLOCK TABLES;
+
 
 --
 -- Table structure for table `DEPENDENT`
@@ -244,9 +320,12 @@ CREATE TABLE `Small_Businesses` (
 --
 
 LOCK TABLES `Small_Businesses` WRITE;
-/*!40000 ALTER TABLE `Small_Businesses` DISABLE KEYS */;
-INSERT INTO `Small_Businesses` VALUES ('Headquarters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
-/*!40000 ALTER TABLE `Small_Businesses` ENABLE KEYS */;
+/*!40000 ALTER TABLE `DEPARTMENT` DISABLE KEYS */;
+INSERT INTO `Small_Businesses` VALUES 
+('123456123416','Sunny Electronics','Electronics',2,'Electricity','B23456123416'),
+('123456123434','Halder Fabrics','Clothes',10,'Electricity','B23456123434'),
+('123456123442','Evergreen Sweets','Food',20,'Ration Subsidy','B23456123442');
+/*!40000 ALTER TABLE `DEPARTMENT` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
